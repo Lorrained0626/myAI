7b0f7b9e2073426cca8529954a6f0407c26cd926
Lorrained0626 <unc.lorraine@gmail.com>
2025-02-28 20:14:35 -0500

    Update ui.ts
    message placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `DollaSense`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Got a financial question? Let's chat!`;
 export const FOOTER_MESSAGE: string = `Customized AI by DiSano`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `Financial Literacy Guide`;
