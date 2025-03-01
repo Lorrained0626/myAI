906516010a82113f6b351b2da8b6e5a8a7b19736
Lorrained0626 <unc.lorraine@gmail.com>
2025-02-28 20:27:17 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -5,3 +5,17 @@ export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble gener
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+
+
+import ToneSelector from "@/components/ToneSelector";
+
+function ChatInterface() {
+    return (
+        <div>
+            <ToneSelector />
+            <ChatBox />
+        </div>
+    );
+}
+
+export default ChatInterface;
