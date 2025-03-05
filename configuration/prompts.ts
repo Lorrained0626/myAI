8adc18f1398b5615676078460b5ee73d839fea39
Lorrained0626 <unc.lorraine@gmail.com>
2025-03-05 15:50:33 -0500

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -47,7 +47,7 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
+Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and her work. Ensure your response is clear, simple, and easy to understand. Avoid technical jargon, break down complex ideas into basic steps, and use real-life examples when possible. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
 
 Excerpts from ${OWNER_NAME}:
 ${context}
